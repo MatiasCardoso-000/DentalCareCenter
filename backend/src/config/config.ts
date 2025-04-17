5a14c89a0360e0f0226cb83f881e1e9e5d749786
MatiasCardoso-000 <matias.cardoso3001@gmail.com>
2025-04-17 14:52:03 -0300

    fix: update default PORT value from 3000 to 4000 in config.ts

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,3 +1,3 @@
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 4000;
 
 export const MONGODB_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/turnodentista'
