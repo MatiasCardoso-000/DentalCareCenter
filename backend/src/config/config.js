e19c0b19651e02348a32c3839626a9218e6f1c17
MatiasCardoso-000 <matias.cardoso3001@gmail.com>
2025-04-18 19:18:46 -0300

    refactor: remove deprecated authentication and database connection code; clean up unused files and schemas

--- backend/src/config/config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGODB_URI = void 0;
-exports.MONGODB_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/turnodentista';
